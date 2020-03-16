cdbca8bd28915b52c62855fcf3d516b694702d4e
Rytautas <31690514+Rytautas@users.noreply.github.com>
2020-03-16 15:49:24 +0200

    application_validation (#2)
    * changed size for phone number in database from 255 to 20 characters
    
    * added validation application
    
    * added validation exception handler for application component

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -9,7 +9,7 @@ CREATE TABLE application (
     id bigint NOT NULL PRIMARY KEY,
     user_id bigint NOT NULL,
     full_name varchar(255) NOT NULL,
-    phone_number varchar(255) NOT NULL,
+    phone_number varchar(20) NOT NULL,
     education varchar(255) NOT NULL,
     free_time varchar(1500) NOT NULL,
     agreement boolean NOT NULL,
