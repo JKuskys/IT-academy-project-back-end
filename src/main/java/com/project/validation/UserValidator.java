92f3e38726dcb23ce15c574b500c6a0756c79637
Milda Petrikaite <Petrikaite.M@gmail.com>
2020-03-16 13:37:42 +0200

    adding user email field validation

--- src/main/java/com/project/validation/UserValidator.java
+++ src/main/java/com/project/validation/UserValidator.java
@@ -4,9 +4,12 @@ import com.project.model.User;
 
 public class UserValidator {
     private MandatoryValueValidator mandatoryValueValidator = new MandatoryValueValidator();
+    private EmailValidator emailValidator = new EmailValidator();
 
     public void validate(User user){
         mandatoryValueValidator.validate(user.getEmail(), "Email");
+        emailValidator.validate(user.getEmail(), "Email");
+
         mandatoryValueValidator.validate(user.getPassword(), "Password");
     }
 }
