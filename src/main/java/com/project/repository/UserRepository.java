0ccc9bb48b5a752532be6869d7e0ed7db821510f
Rytautas <rytautas.kvasinskas@gmail.com>
2020-03-18 15:42:42 +0200

    added authentication base with jwt token changes to database (user is_admin field replaced with roles field) model/user now implements UserDetails interface added model/AuthenticationRequest model
    bugs:
    for some reason denies access to /auth/login POST method even though its permitted
    does not load up applications

--- src/main/java/com/project/repository/UserRepository.java
+++ src/main/java/com/project/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.project.repository;
 
+import com.project.exception.UserNotFoundException;
 import com.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email) throws UsernameNotFoundException;
 }
