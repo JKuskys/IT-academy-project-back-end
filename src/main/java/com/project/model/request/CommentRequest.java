0d6f90c1396415f4da3ec09608b06e85e4a32e06
Rytautas <rytautas.kvasinskas@gmail.com>
2020-03-24 18:27:51 +0200

    naming change

---
+++ src/main/java/com/project/model/request/CommentRequest.java
@@ -0,0 +1,19 @@
+package com.project.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequest {
+
+    private String comment;
+
+    private String commentDate;
+
+    private long applicationId;
+
+    private String authorEmail;
+}
