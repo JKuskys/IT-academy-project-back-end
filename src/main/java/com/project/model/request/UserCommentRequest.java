75100ebccf390ce0c379a9fe9e78e3c9b1ba82e7
Rytautas <31690514+Rytautas@users.noreply.github.com>
2020-03-26 13:21:43 +0200

    Feat/user application get (#27)
    * adding email validation tests
    
    * adding password validation tests
    
    * adding mandatory field validation tests
    
    * adding full user validation tests
    
    * changed naming of a few fields in model/Application class (from application_date to applicationDate etc.)
    
    * added authenticationResponse model
    changed comment REST controller paths to include application id
    comment REST controllers now work for specific applications
    replaced for each loops with more efficient stream mapping
    
    * moved value validation from database entity models to request entity models
    changed exception and validation messages to use String.format()
    
    * fixed requiring comment author fullName and email to post a new comment
    
    * fixed requiring comment author fullName and email to post a new comment
    
    * getting a users application by email
    
    * fixed validation bug
    
    * Added functionality to fetch specific users application by email
    
    Co-authored-by: Milda Petrikaite <Petrikaite.M@gmail.com>

---
+++ src/main/java/com/project/model/request/UserCommentRequest.java
@@ -0,0 +1,12 @@
+package com.project.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCommentRequest {
+    private String email;
+}
