f07ed51fe0cd9f689291deb7b3d109e62b00cb09
Rytautas <rytautas.kvasinskas@gmail.com>
2020-03-31 15:32:57 +0300

    created custom annotation for validating password added @Valid annotation on fields where it was necessary and missed

--- src/main/java/com/project/model/request/ApplicationUpdateRequest.java
+++ src/main/java/com/project/model/request/ApplicationUpdateRequest.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApplicationUpdateRequest {
+    @NotBlank(message = "Statusas negali būti tuščias")
     private ApplicationStatus status;
 }
