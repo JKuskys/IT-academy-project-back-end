5338f3b26b4cf210bf984a89775b6bb7a58cd64a
Rytautas <rytautas.kvasinskas@gmail.com>
2020-04-08 13:08:58 +0300

    added password reset controller to manage password reset tokens fixed one test failing due to changes with notifications

---
+++ src/main/java/com/project/repository/PasswordTokenRepository.java
@@ -0,0 +1,8 @@
+package com.project.repository;
+
+import com.project.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    PasswordResetToken findByToken(String token);
+}
